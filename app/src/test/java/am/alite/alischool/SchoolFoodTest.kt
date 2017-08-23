100b2c600ddc5737c79d5f2e1d41e3bb30f7453f
AxelMatstoms <axel.matstoms@gmail.com>
2017-08-23 17:20:14 +0200

    Added tests for school food

--- app/src/test/java/am/alite/alischool/SchoolFoodTest.kt
+++ app/src/test/java/am/alite/alischool/SchoolFoodTest.kt
@@ -1,7 +1,34 @@
 package am.alite.alischool
 
+import am.alite.alischool.vo.schoolfood.FoodItem
+import org.junit.Test
+import org.junit.Assert.*
+
 /**
  * Created by axm on 22/08/17.
  */
 class SchoolFoodTest {
+    @Test
+    fun testFoodItemParsingName() {
+        val item = FoodItem("Chicken Madras, rice (Organic, Healthy)")
+        assertEquals("Chicken Madras, rice", item.name)
+    }
+
+    @Test
+    fun testFoodItemParsingTags() {
+        val item = FoodItem("Chicken Madras, rice (Organic, Healthy)")
+        assertEquals(listOf("Organic", "Healthy"), item.tags)
+    }
+
+    @Test
+    fun testFoodItemParsingNoTags() {
+        val item = FoodItem("Chicken Madras, rice")
+        assertEquals(listOf<String>(), item.tags)
+    }
+
+    @Test
+    fun testFoodItemParsingOneTag() {
+        val item = FoodItem("Chicken Madras, rice (Organic)")
+        assertEquals(listOf("Organic"), item.tags)
+    }
 }
